f468fcd62f4aa626a946fb16b3a6a6617af76e7e
Federico <fedegmz18@gmail.com>
2022-04-05 18:45:28 -0300

    corrected ifndef in .h

--- tp1_escape_pokemon/src/string_a_bool.h
+++ tp1_escape_pokemon/src/string_a_bool.h
@@ -1,9 +1,9 @@
-#ifndef VERIFICAR_BOOL_H_
-#define VERIFICAR_BOOL_H_
+#ifndef STRING_A_BOOL_H_
+#define STRING_A_BOOL_H_
 
 /*
- * Dada una cadena, deuvelve el valor correspondiente a entero o -1 
- * si no es una cadena interpretable como bool.
+ * Dada una cadena, deuvelve el valor correspondiente a booleano (en formato de entero, es decir 0 o 1)
+ * o -1 si no es una cadena interpretable como bool.
  */
 int string_a_bool(char *string);
 
