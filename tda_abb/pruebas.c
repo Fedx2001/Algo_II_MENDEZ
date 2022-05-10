e61a7d24b01b160583604c4ec6503b52b6e22e6b
Fede gmz <fedgomez@MacBook-Pro-de-Javier.local>
2022-05-10 15:45:31 -0300

    abb files

--- tda_abb/pruebas.c
+++ tda_abb/pruebas.c
@@ -5,7 +5,15 @@
 
 int main()
 {
-  pa2m_nuevo_grupo("Pruebas de ABB");
+	pa2m_nuevo_grupo("Pruebas de ABB");
+	pa2m_nuevo_grupo("Prueba de creación");
+	puedo_crear_un_abb();
 
-  return pa2m_mostrar_reporte();
+	pa2m_nuevo_grupo("Pruebas de Inserción");
+	insertar_en_abb_vacio_inserta_en_la_raiz();
+	insertar_un_elemento_mayor_que_la_raiz_inserta_en_la_derecha_de_la_raiz();
+	insertar_un_elemento_menor_que_la_raiz_inserta_en_la_izquierda_de_la_raiz();
+
+
+	return pa2m_mostrar_reporte();
 }
