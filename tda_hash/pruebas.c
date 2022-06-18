fe453cc17636735152514bc1165d8cf96fb5eda1
Federico <fedegmz18@gmail.com>
2022-06-18 13:11:12 -0300

    Added tests for iterator and finders

--- tda_hash/pruebas.c
+++ tda_hash/pruebas.c
@@ -50,7 +50,7 @@ void puedo_insertar_varios_elementos()
 	hash_t *hash = hash_crear(4);
 	hash_t *resultado = NULL;
 	char elementos[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-	
+
 	for(int i = 0; i < 9; i++){
 		resultado = hash_insertar(hash, "a"+i, elementos+i, NULL);
 		if(!resultado) break;
@@ -97,10 +97,10 @@ void insertar_una_clave_con_mismo_hash_que_otra_no_reemplaza_la_clave_existente(
 	hash_t *hash = hash_crear(4);
 	int elementos[2] = {1, 10};
 
-	hash_insertar(hash, "AAAA0001", elementos, NULL);
-	hash_insertar(hash, "AAAA0010", elementos+1, NULL);
+	hash_insertar(hash, "A", elementos, NULL);
+	hash_insertar(hash, "B", elementos+1, NULL);
 
-	pa2m_afirmar(hash_obtener(hash, "AAAA0001") == elementos && hash_obtener(hash, "AAAA0010") == elementos+1, 
+	pa2m_afirmar(hash_obtener(hash, "A") == elementos && hash_obtener(hash, "B") == elementos+1,
 				  "Insertar dos claves con el mismo valor de hash no reemplaza la clave ya existente");
 
 	hash_destruir(hash);
@@ -165,16 +165,14 @@ void puedo_quitar_una_clave_existente_en_el_hash()
 
 void quitar_una_clave_actualiza_la_cantidad_de_elementos()
 {
-	hash_t *hash = hash_crear(4);
-	char *elementos[4] = {"Nadie:", "Absolutamente nadie:", 
+	hash_t *hash = hash_crear(10);
+	char *elementos[4] = {"Nadie:", "Absolutamente nadie:",
 			      "Alumnos de Algo2 haciendo los tps:", "https://youtu.be/YWaq5Opv6Wg"};
 	char clave[11] = "AAAAAAAAAA";
-	char letra = 65;
 
 	for(int i = 0; i < 4; i++){
-		clave[10] = letra;
 		hash_insertar(hash, clave, elementos+i, NULL);
-		letra++;
+		clave[9]++;
 	}
 
 	hash_quitar(hash, "AAAAAAAAAA");
@@ -185,12 +183,98 @@ void quitar_una_clave_actualiza_la_cantidad_de_elementos()
 }
 
 
-void no_puedo_obtener_en_un_hash_NULL();
-void no_puedo_obtener_un_elemento_con_clave_NULL();
-void no_puedo_obtener_un_elemento_con_clave_no_presente_en_hash();
-void no_puedo_usar_hash_contiene_con_hash_NULL();
-void un_hash_no_contiene_clave_NULL();
-void un_hash_no_contiene_elemento_con_clave_no_presente_en_el_hash();
+void no_puedo_obtener_un_elemento_en_hash_NULL()
+{
+	pa2m_afirmar(hash_obtener(NULL, "AAAA") == NULL, "No puedo obtener un elemento de un hash NULL");
+}
+
+void no_puedo_obtener_un_elemento_con_clave_NULL()
+{
+	hash_t *hash = hash_crear(4);
+
+	pa2m_afirmar(hash_obtener(hash, NULL) == NULL, "No puedo obtener un elemento con clave NULL");
+
+	hash_destruir(hash);
+}
+
+void no_puedo_obtener_un_elemento_con_clave_no_presente_en_hash()
+{
+	hash_t *hash = hash_crear(4);
+
+	pa2m_afirmar(hash_obtener(hash, "AAAA") == NULL, 
+				  "Obtener una clave no existente en el hash no encuentra un elemento");
+
+	hash_destruir(hash);
+}
+
+void no_puedo_usar_hash_contiene_con_hash_NULL()
+{
+	pa2m_afirmar(hash_contiene(NULL, "AAAA") == false, "No puedo usar hash_contiene con hash NULL");
+}
+
+void no_puedo_usar_hash_contiene_con_clave_NULL()
+{
+	hash_t *hash = hash_crear(4);
+	
+	pa2m_afirmar(hash_contiene(hash, NULL) == false, "Un hash no puede contener clave NULL");
+
+	hash_destruir(hash);
+}
+
+void un_hash_no_contiene_elemento_con_clave_no_presente_en_el_hash()
+{
+	hash_t *hash = hash_crear(4);
+
+	pa2m_afirmar(hash_contiene(hash, "NULL") == false, 
+		     "No existe elemento con una clave no insertada en el hash");
+
+	hash_destruir(hash);
+}
+
+
+bool comparador_enteros(const char *clave, void *valor, void *aux)
+{
+	return true;
+}
+
+bool siempre_true(const char *clave, void *valor, void *aux)
+{
+	return true;
+}
+
+void con_cada_clave_en_hash_NULL_devuelve_0_elementos_iterados()
+{
+	pa2m_afirmar(hash_con_cada_clave(NULL, comparador_enteros, NULL) == 0, 
+		     "Iterador con hash NULL devuelve 0 claves iteradas");
+}
+
+void con_cada_clave_con_funcion_NULL_devuelve_0_elementos_iterados()
+{
+	hash_t *hash = hash_crear(4);
+
+	pa2m_afirmar(hash_con_cada_clave(hash, NULL, NULL) == 0, 
+		     "Iterador con funcion NULL devuelve 0 claves iteradas");
+
+	hash_destruir(hash);
+}
+
+void iterar_con_funcion_siempre_true_itera_todas_las_claves()
+{
+	hash_t *hash = hash_crear(4);
+
+	char *elementos[4] = {"BABY", "BABY", "BABY", "https://youtu.be/bm-BOFvghrI"};
+	char clave[11] = "WAAAAAAAAH";
+
+	for(int i = 0; i < 4; i++){
+		hash_insertar(hash, clave, elementos+i, NULL);
+		clave[9]++;
+	}
+
+	pa2m_afirmar(hash_con_cada_clave(hash, siempre_true, NULL) == 4, 
+		     "Iterador con funcion siempre true itera todas las claves");
+	
+	hash_destruir(hash);
+}
 
 int main()
 {
@@ -199,6 +283,7 @@ int main()
 	pa2m_nuevo_grupo("Pruebas de creacion");
 	crear_un_hash_con_capacidad_menor_a_3_crea_un_hash_con_capacidad_3();
 	crear_un_hash_de_capacidad_mayor_a_3_crea_el_hash_correctamente();
+
 
 	pa2m_nuevo_grupo("Pruebas de insercion");
 	no_puedo_insertar_en_un_hash_NULL();
@@ -220,9 +305,18 @@ int main()
 
 
 	pa2m_nuevo_grupo("Pruebas de hash_contiene y hash_obtener");
+	no_puedo_obtener_un_elemento_en_hash_NULL();
+	no_puedo_obtener_un_elemento_con_clave_NULL();
+	no_puedo_obtener_un_elemento_con_clave_no_presente_en_hash();
+	no_puedo_usar_hash_contiene_con_hash_NULL();
+	no_puedo_usar_hash_contiene_con_clave_NULL();
+	un_hash_no_contiene_elemento_con_clave_no_presente_en_el_hash();
 
 
 	pa2m_nuevo_grupo("Pruebas con cada clave");
+	con_cada_clave_en_hash_NULL_devuelve_0_elementos_iterados();
+	con_cada_clave_con_funcion_NULL_devuelve_0_elementos_iterados();
+	iterar_con_funcion_siempre_true_itera_todas_las_claves();
 
 
 	return pa2m_mostrar_reporte();
